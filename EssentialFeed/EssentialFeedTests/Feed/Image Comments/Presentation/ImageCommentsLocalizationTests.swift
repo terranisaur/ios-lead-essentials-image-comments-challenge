cd0c7976889706898aa9aaf42fcfbb6be88e812b
Alex Thurston <athurston@icsusa.com>
2021-08-13 14:28:39 -0500

    Implement ImageCommentsLocalizationTests and add relevant translation to ImageComments.string

--- EssentialFeed/EssentialFeedTests/Feed/Image Comments/Presentation/ImageCommentsLocalizationTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Image Comments/Presentation/ImageCommentsLocalizationTests.swift
@@ -5,4 +5,11 @@
 import XCTest
 import EssentialFeed
 
-class ImageCommentsLocalizationTests: XCTestCase {}
+final class ImageCommentsLocalizationTests: XCTestCase {
+	func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+		let table = "ImageComments"
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+		assertLocalizedKeyAndValuesExist(in: bundle, table)
+	}
+}
--- EssentialFeed/EssentialFeedTests/Feed/Image Comments/Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Image Comments/Presentation/ImageCommentsPresenterTests.swift
@@ -1,4 +1,4 @@
-//	
+//
 // Copyright © 2021 Essential Developer. All rights reserved.
 //
 
@@ -6,27 +6,27 @@ import XCTest
 import EssentialFeed
 
 class ImageCommentsPresenterTests: XCTestCase {
-    func test_title_isLocalized() {
-        XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_TITLE"))
-    }
+	func test_title_isLocalized() {
+		XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_TITLE"))
+	}
 
-    func test_map_createsViewModel() {
-        let feed = uniqueImageFeed().models
+	func test_map_createsViewModel() {
+		let feed = uniqueImageFeed().models
 
-        let viewModel = FeedPresenter.map(feed)
+		let viewModel = FeedPresenter.map(feed)
 
-        XCTAssertEqual(viewModel.feed, feed)
-    }
+		XCTAssertEqual(viewModel.feed, feed)
+	}
 
-    // MARK: - Helpers
+	// MARK: - Helpers
 
-    private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
-        let table = "ImageComments"
-        let bundle = Bundle(for: ImageCommentsPresenter.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
-        }
-        return value
-    }
+	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+		let table = "ImageComments"
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
+		let value = bundle.localizedString(forKey: key, value: nil, table: table)
+		if value == key {
+			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+		}
+		return value
+	}
 }
