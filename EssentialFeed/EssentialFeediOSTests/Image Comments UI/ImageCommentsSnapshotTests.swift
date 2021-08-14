fc8d53659b78080d38457f2eefc584cb078254fb
Alex Thurston <athurston@icsusa.com>
2021-08-14 11:05:19 -0500

    Add ImageComments snapshot tests for Loading state

--- EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -3,7 +3,28 @@
 //
 
 import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
 
 class ImageCommentsSnapshotTests: XCTestCase {
-	func test() {}
+	func test_loadingComments() {
+		let sut = makeSUT()
+
+		sut.display(ResourceLoadingViewModel(isLoading: true))
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "COMMENTS_LOADING_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "COMMENTS_LOADING_dark")
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT() -> ListViewController {
+		let bundle = Bundle(for: ListViewController.self)
+		let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+		let controller = storyboard.instantiateInitialViewController() as! ListViewController
+		controller.loadViewIfNeeded()
+		controller.tableView.showsVerticalScrollIndicator = false
+		controller.tableView.showsHorizontalScrollIndicator = false
+		return controller
+	}
 }
