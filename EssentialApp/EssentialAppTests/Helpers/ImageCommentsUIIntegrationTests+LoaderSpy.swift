b5c9057adfc0eda959693d879b74bee740fce90c
Alex Thurston <athurston@icsusa.com>
2021-08-20 14:43:17 -0500

    Copy FeedUIIntegration tests into ImageCommentsUIIntegrationTests as a baseline for the tests

---
+++ EssentialApp/EssentialAppTests/Helpers/ImageCommentsUIIntegrationTests+LoaderSpy.swift
@@ -0,0 +1,34 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+import EssentialFeediOS
+import Combine
+
+extension ImageCommentsUIIntegrationTests {
+	class LoaderSpy {
+		private var commentsRequests = [PassthroughSubject<[ImageComment], Error>]()
+
+		var loadCommentsCallCount: Int {
+			return commentsRequests.count
+		}
+
+		func loadPublisher() -> AnyPublisher<[ImageComment], Error> {
+			let publisher = PassthroughSubject<[ImageComment], Error>()
+			commentsRequests.append(publisher)
+			return publisher.eraseToAnyPublisher()
+		}
+
+		func completeCommentsLoading(with comments: [ImageComment] = [], at index: Int = 0) {
+			commentsRequests[index].send(comments)
+			// requests[index].send(completion: .finished) ?
+		}
+
+		func completeCommentsLoadingWithError(at index: Int = 0) {
+			let error = NSError(domain: "an error", code: 0)
+			commentsRequests[index].send(completion: .failure(error))
+		}
+	}
+}
