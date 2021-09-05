5af8092935da589d154e86ac27650c8f60f255a1
Alex Thurston <athurston@bleachr.co>
2021-09-05 15:18:40 -0500

    Remove dateFormatter usage in ImageCommentsMapperTests and replace it with hard coded values

--- EssentialFeed/EssentialFeedTests/Feed/Image Comments/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Image Comments/API/ImageCommentsMapperTests.swift
@@ -41,15 +41,14 @@ class ImageCommentsMapperTests: XCTestCase {
 	}
 
 	func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws {
-		let date = Date()
 		let item1 = makeItem(id: UUID(),
 		                     message: "a comment",
-		                     creationTime: date,
+		                     creationTime: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"),
 		                     authorName: "an author")
 
 		let item2 = makeItem(id: UUID(),
 		                     message: "another comment",
-		                     creationTime: date,
+		                     creationTime: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"),
 		                     authorName: "another author")
 
 		let json = makeItemsJSON([item1.json, item2.json])
@@ -61,15 +60,12 @@ class ImageCommentsMapperTests: XCTestCase {
 
 	// MARK: - Helpers
 
-	private func makeItem(id: UUID, message: String, creationTime: Date, authorName: String) -> (model: ImageComment, json: [String: Any]) {
-		let formatter = ISO8601DateFormatter()
-		let iso8601date = formatter.date(from: formatter.string(from: creationTime))!
-		let item = ImageComment(id: id, message: message, creationTime: iso8601date, authorName: authorName)
-
+	private func makeItem(id: UUID, message: String, creationTime: (date: Date, iso8601String: String), authorName: String) -> (model: ImageComment, json: [String: Any]) {
+		let item = ImageComment(id: id, message: message, creationTime: creationTime.date, authorName: authorName)
 		let json = [
 			"id": id.uuidString,
 			"message": message,
-			"created_at": formatter.string(from: iso8601date),
+			"created_at": creationTime.iso8601String,
 			"author": [
 				"username": authorName
 			]
