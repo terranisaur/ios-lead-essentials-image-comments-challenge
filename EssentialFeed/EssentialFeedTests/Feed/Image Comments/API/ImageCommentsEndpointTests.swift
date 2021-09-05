3d59d251d86bce19c7b3fe8fc9a62304aa965a31
Alex Thurston <athurston@bleachr.co>
2021-09-05 15:13:08 -0500

    Remove extra logic in tests by providing a uuidString directly and removing string interpolation

--- EssentialFeed/EssentialFeedTests/Feed/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -9,9 +9,9 @@ class ImageCommentsEndpointTests: XCTestCase {
 	func test_comments_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
 
-		let uuid = UUID()
+		let uuid = UUID(uuidString: "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086")!
 		let received = ImageCommentsEndpoint.get.url(baseURL: baseURL, imageId: uuid)
-		let expected = URL(string: "http://base-url.com/v1/image/\(uuid.uuidString)/comments")!
+		let expected = URL(string: "http://base-url.com/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments")!
 
 		XCTAssertEqual(received, expected)
 	}
